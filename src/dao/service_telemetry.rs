f694cecfdc91775a5b4f0e3df611f30eea5f098e
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-01 04:20:11 +0100

    wip: transactional insert

--- src/dao/service_telemetry.rs
+++ src/dao/service_telemetry.rs
@@ -1,35 +1,44 @@
-use sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, FromQueryResult, QuerySelect, Set};
+use migration::DbErr;
+use sea_orm::{
+    ActiveModelTrait, ColumnTrait, EntityTrait, FromQueryResult, QuerySelect, Set, TransactionTrait,
+};
 
 use crate::entity::telemetry;
 use crate::{controller::controller_storage::KeyValue, DB_POOL};
 pub async fn store_data_db(data: Vec<KeyValue>) -> bool {
     let db = DB_POOL.get().await;
 
-    let max_opt = telemetry::Entity::find()
-        .select_only()
-        .column_as(telemetry::Column::RequestId.max(), "value")
-        .into_model::<SingleNumeric>()
-        .one(db)
-        .await
-        .unwrap_or(Some(SingleNumeric { value: 0 }));
+    db.transaction::<_, bool, DbErr>(|txn| {
+        Box::pin(async move {
+            let max_opt = telemetry::Entity::find()
+                .select_only()
+                .column_as(telemetry::Column::RequestId.max(), "value")
+                .into_model::<SingleNumeric>()
+                .one(txn)
+                .await
+                .unwrap_or(Some(SingleNumeric { value: 0 }));
 
-    let max = max_opt.unwrap().value + 1;
+            let max = max_opt.unwrap().value + 1;
 
-    for item in data {
-        let active_model = telemetry::ActiveModel {
-            code: Set(item.code),
-            key: Set(item.key),
-            value: Set(item.value),
-            request_id: Set(max),
-            ..Default::default()
-        };
+            for item in data {
+                let active_model = telemetry::ActiveModel {
+                    code: Set(item.code),
+                    key: Set(item.key),
+                    value: Set(item.value),
+                    request_id: Set(max),
+                    ..Default::default()
+                };
 
-        let result = active_model.insert(db).await;
-        if result.is_err() {
-            return false;
-        }
-    }
-    return true;
+                let result = active_model.insert(txn).await;
+                if result.is_err() {
+                    return Ok(false);
+                }
+            }
+            return Ok(true);
+        })
+    })
+    .await
+    .unwrap_or(false)
 }
 
 #[derive(FromQueryResult)]
