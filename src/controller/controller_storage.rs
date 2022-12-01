db56edf4b1b682c5a14b3fc0528c806fc2091272
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-01 02:47:24 +0100

    wip

--- src/controller/controller_storage.rs
+++ src/controller/controller_storage.rs
@@ -2,11 +2,25 @@ use super::controller_common;
 use serde::{Deserialize, Serialize};
 use warp::{Rejection, Reply};
 
-pub async fn store_data(name: String, esempio: Esempio) -> Result<impl Reply, Rejection> {
-    return controller_common::generate_response(Ok(format!("{} {}", name, esempio.last_name)));
+pub async fn store_data(esempio: Esempio) -> Result<impl Reply, Rejection> {
+    return controller_common::generate_response(Ok(generate_string(esempio.data)));
+}
+
+fn generate_string(esempio: Vec<KeyValue>) -> String {
+    let mut result = "".to_string();
+    for tuple in esempio {
+        result = format!("key={}, value={}; {}", tuple.key, tuple.value, result);
+    }
+    return result.to_owned();
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Esempio {
-    last_name: String,
+    data: Vec<KeyValue>,
+}
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct KeyValue {
+    code: String,
+    key: String,
+    value: String,
 }
