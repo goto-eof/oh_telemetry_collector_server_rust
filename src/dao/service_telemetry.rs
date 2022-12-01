938282b42fbdef47f9f35e643841c4bb13f1b646
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-01 04:14:53 +0100

    wip: retrieve max request_id for new inserts

--- src/dao/service_telemetry.rs
+++ src/dao/service_telemetry.rs
@@ -1,16 +1,26 @@
-use sea_orm::{entity, ActiveModelTrait, Set};
+use sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, FromQueryResult, QuerySelect, Set};
 
 use crate::entity::telemetry;
 use crate::{controller::controller_storage::KeyValue, DB_POOL};
 pub async fn store_data_db(data: Vec<KeyValue>) -> bool {
     let db = DB_POOL.get().await;
 
+    let max_opt = telemetry::Entity::find()
+        .select_only()
+        .column_as(telemetry::Column::RequestId.max(), "value")
+        .into_model::<SingleNumeric>()
+        .one(db)
+        .await
+        .unwrap_or(Some(SingleNumeric { value: 0 }));
+
+    let max = max_opt.unwrap().value + 1;
+
     for item in data {
         let active_model = telemetry::ActiveModel {
             code: Set(item.code),
             key: Set(item.key),
             value: Set(item.value),
-            request_id: Set(1),
+            request_id: Set(max),
             ..Default::default()
         };
 
@@ -20,4 +30,9 @@ pub async fn store_data_db(data: Vec<KeyValue>) -> bool {
         }
     }
     return true;
+}
+
+#[derive(FromQueryResult)]
+struct SingleNumeric {
+    value: i32,
 }
