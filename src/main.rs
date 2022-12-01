a5c2a58a8a2fc2fd8e2df6a2b49fe34aaa2a2c24
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-01 01:42:22 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ mod route;
 
 #[tokio::main]
 async fn main() {
-    println!("server started :)");
+    println!("================================");
+    println!("server started on port [8013] :)");
+    println!("================================");
+
     warp::serve(init_routes().await)
         .run(([0, 0, 0, 0], 8013))
         .await;
