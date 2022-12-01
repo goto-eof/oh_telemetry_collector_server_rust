e9388bf04b70c6ff2bd6e88c26f94ad1771d9c2e
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-01 02:05:11 +0100

    wip

--- src/controller/controller_storage.rs
+++ src/controller/controller_storage.rs
@@ -1,6 +1,12 @@
 use super::controller_common;
+use serde::{Deserialize, Serialize};
 use warp::{Rejection, Reply};
 
-pub async fn store_data(name: String) -> Result<impl Reply, Rejection> {
-    return controller_common::generate_response(Ok(name));
+pub async fn store_data(name: String, esempio: Esempio) -> Result<impl Reply, Rejection> {
+    return controller_common::generate_response(Ok(format!("{} {}", name, esempio.last_name)));
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct Esempio {
+    last_name: String,
 }
--- src/route/routes_telemetry.rs
+++ src/route/routes_telemetry.rs
@@ -3,8 +3,9 @@ use warp::{Filter, Rejection, Reply};
 
 pub fn get_telemetry_routes() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     let routes = warp::path("telemetry")
-        .and(warp::get())
+        .and(warp::post())
         .and(warp::path::param::<String>())
+        .and(warp::body::json())
         .and(warp::path::end())
         .and_then(controller_storage::store_data);
 
