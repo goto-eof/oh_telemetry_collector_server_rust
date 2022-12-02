02ada56b6960c70d08eb15ed36261495d9a0186c
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-02 13:11:54 +0100

    doc: update

--- README.md
+++ README.md
@@ -8,8 +8,17 @@ docker-compose up
 
 ### use postman collection for making requests
 
-### The server responds on
+### The server responds on
 
 ```
 http://localhost:8013/telemetry
+```
+
+
+### DB connection
+
+```
+postgres://127.0.0.1:5432/postgres
+username: postgres
+password: postgres
 ```
--- src/controller/controller_telemetry.rs
+++ src/controller/controller_telemetry.rs
@@ -1,7 +1,16 @@
 use super::{controller_common, request::telemetry_insert_request::TelemetryInsertrequest};
 use crate::dao::service_telemetry::store_data_db;
+use log::debug;
 use warp::{Rejection, Reply};
 
-pub async fn store_data(esempio: TelemetryInsertrequest) -> Result<impl Reply, Rejection> {
-    return controller_common::generate_response(Ok(store_data_db(esempio.data).await));
+pub async fn store_data(
+    telemetry_insert_request: TelemetryInsertrequest,
+) -> Result<impl Reply, Rejection> {
+    debug!(
+        "received request for store_data(): {:?}",
+        &telemetry_insert_request
+    );
+    return controller_common::generate_response(Ok(
+        store_data_db(telemetry_insert_request.data).await
+    ));
 }
