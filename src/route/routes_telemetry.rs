93065cfa8a4451d46070ff71c2c5fb5544d1e6e2
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-05 02:43:00 +0100

    chore

--- src/route/routes_telemetry.rs
+++ src/route/routes_telemetry.rs
@@ -2,7 +2,7 @@ use crate::controller::controller_telemetry;
 use warp::{Filter, Rejection, Reply};
 
 pub fn get_telemetry_routes() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    let routes = warp::path("telemetry")
+    let routes = warp::path("collect")
         .and(warp::post())
         .and(warp::body::json())
         .and(warp::path::end())
